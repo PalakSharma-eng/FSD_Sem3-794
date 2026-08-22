7bd0a89bde3b1a5d4e49553a1e33fdcfd0cab15d
PalakSharma-eng <palaksoti01@gmail.com>
2026-08-22 14:12:34 +0530

    js

---
+++ time.js
@@ -0,0 +1,37 @@
+// console.log("one");
+// console.log("two");
+// console.log("three");
+
+// setTimeout(function(){
+//     console.log("Hello after 5 seconds");
+// }, 5000);
+
+// console.log("four");
+// console.log("five");
+
+// setTimeout(function(){
+//     console.log("Hello after 3 seconds");
+// }, 3000);
+
+// function greet(f_name, l_name){
+//     console.log("Hello " + f_name + " " + l_name);
+// }
+
+// setTimeout(greet, 2000, "John", "Doe");
+
+
+1: Passing an array to Function
+function displayNumbers(numbers){
+    console.log(numbers);
+}
+
+let arr = [1, 2, 3, 4, 5];
+displayNumbers(arr);
+
+function displayStudents(students){
+    console.log(student.name);
+    console.log(student.age);
+}
+
+let student = {name: "John", age: 20};
+displayStudents(student);
