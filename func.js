31110407a9beef7eb707167aa2b94fafd8a0e917
PalakSharma-eng <palaksoti01@gmail.com>
2026-08-22 14:11:07 +0530

    js

---
+++ func.js
@@ -0,0 +1,18 @@
+// function a_func(a,b){
+//     let s,m,mu,rem;
+//     s= a+b;
+//     m= a-b;
+//     mu= a*b;
+//     rem= a%b;
+// }
+
+a_func(10,5);
+let r= function(a,b)
+    {console.log(a+b)};
+r(10,5);
+
+
+
+function greet(name){
+    console.log
+}
